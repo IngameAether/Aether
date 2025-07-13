54a94d7cb8d1deb0e9ba79347f872a7c79e66896
watermell0n <wu2078@naver.com>
2025-07-14 04:19:26 +0900

    feat: sow TowerInfoDisplay
    타워 클릭시 정보창 표시

--- Assets/Scripts/Map/TileInteraction.cs
+++ Assets/Scripts/Map/TileInteraction.cs
@@ -8,6 +8,7 @@ public class TileInteraction : MonoBehaviour
     public static GameObject[] staticElementPrefabs;  // 전역 변수로 선언(모든 tile이 공유할 내용이므로)
     public static GameObject[] staticTowerPrefabs;
     public static int clickNum = 0;  // 전체에서 클릭 횟수를 공유해야 하므로 static 선언
+    public static bool isTowerJustCreated = false;  // 타일을 클릭해 타워가 배치된 것인지 타워를 클릭한 것인지 구분하기 위해
 
     void Start()
     {
@@ -23,11 +24,13 @@ public class TileInteraction : MonoBehaviour
 
         if (!tile.isBuild || !tile.isElementBuild) return;
 
+        // 타일 첫 클릭은 타워 배치되게: 나중에 되돌릴 예정
         int ranNum = 0; GameObject elementObj = null;
         if (clickNum == 0)
         {
             elementObj = Instantiate(staticTowerPrefabs[0], tile.transform.position, Quaternion.identity);
             clickNum++;
+            isTowerJustCreated = true;
         }
         else
         {
@@ -37,7 +40,7 @@ public class TileInteraction : MonoBehaviour
         }
         //int ranNum = Random.Range(0, staticElementPrefabs.Length);
         //GameObject elementObj = Instantiate(staticElementPrefabs[ranNum], tile.transform.position, Quaternion.identity);
-        
+
         // 원소가 배치된 타일 저장
         ElementController ec = elementObj.GetComponent<ElementController>();
         if (ec != null) ec.selectTile = tile;
