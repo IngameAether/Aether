4e42e3ffee038cded734ffef1067ad77a4f558ec
watermell0n <wu2078@naver.com>
2025-08-19 18:12:43 +0900

    add: Enemy Data

---
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "TowerDefense/EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public string ID { get; private set; }
+    [field: SerializeField] public float HP { get; private set; }
+    [field: SerializeField] public float Speed { get; private set; }
+    [field: SerializeField] public float DamageReduction { get; private set; }
+    [field: SerializeField] public float ControlResistance { get; private set; }
+    [field: SerializeField] public int Aether {  get; private set; }
+    [field: SerializeField] public string Description { get; private set; }
+}
