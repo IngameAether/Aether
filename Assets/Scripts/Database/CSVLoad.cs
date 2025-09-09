c53707466121617b86f9fd6e8d7591d7592c9f85
watermell0n <wu2078@naver.com>
2025-09-09 20:04:56 +0900

    feat: apply Enemy_Data to Enemy info
    Enemy_Data csv 파일에서 데이터 읽어 와 enemy speed 등 적용

--- Assets/Scripts/Database/CSVLoad.cs
+++ Assets/Scripts/Database/CSVLoad.cs
@@ -8,5 +8,10 @@ public class CSVLoad : MonoBehaviour
     {
         List<Dictionary<string, object>> enemyData = CSVReader.Read("Enemy_Data");
         List<Dictionary<string, object>> towerData = CSVReader.Read("Tower_Data");
+
+        //for (int i=0; i<enemyData.Count; i++)
+        //{
+        //    print(enemyData[i]["identification_code"].ToString());
+        //}
     }
 }
